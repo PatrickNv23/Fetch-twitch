50e948de0a0144bb622f90ef014182fbab112f72
Patrick_Nv <patricknv230101@gmail.com>
2022-09-22 01:04:19 -0500

    Explicacion fetch en twitch

---
+++ fetch.js
@@ -0,0 +1,43 @@
+const btn_obtenerUsuarios = document.querySelector(".btn_obtenerUsuarios");
+const listaUsuarios = document.getElementById("lista_usuarios");
+const fragmentoUsuarios = document.createDocumentFragment();
+
+const obtenerUsuarios = async () => {
+    try {
+        //listaUsuarios.innerHTML = "";
+        const usuarios = await fetch("https://jsonplaceholder.typicode.com/users");
+        const usuarios_json = await usuarios.json();
+
+        if (!usuarios.ok) {
+            throw { status: usuarios.status, statusText: usuarios.statusText }
+        }
+
+        usuarios_json.forEach((usuario) => {
+            const usuarioHtml = document.createElement("li");
+            usuarioHtml.classList.add("usuario_item");
+            usuarioHtml.innerHTML =
+                `
+        <strong class="item" >Id:</strong> ${usuario.id} - <strong class="item">Nombre:</strong> ${usuario.name}
+        <br />
+        <strong class="item">Email: </strong> ${usuario.email}
+        `;
+
+            fragmentoUsuarios.appendChild(usuarioHtml);
+        });
+
+        listaUsuarios.appendChild(fragmentoUsuarios);
+    } catch (error) {
+        let mensaje = error.statusText || "Ocurrió un error";
+        const errorApi = document.createElement("li");
+        errorApi.innerHTML = `Error ${error.status}: ${mensaje}`;
+        listaUsuarios.appendChild(errorApi);
+    } finally {
+        console.log("Evento culminado");
+    }
+
+}
+
+btn_obtenerUsuarios.addEventListener("click", (e) => {
+    obtenerUsuarios();
+})
+
